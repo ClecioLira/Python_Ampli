a792050f4b6c3a446c68a2d773f91dcc59582751
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-12-19 09:41:40 -0300

    update

--- estrutura_de_dados.py
+++ estrutura_de_dados.py
@@ -51,3 +51,45 @@
 # print("lista[4][1] = ", lista[4][1])
 
 # COMPREENSÃO DE LISTA
+# linguagens = ['Python', 'Java', 'JavaScript', 'C', 'C++', 'Swift', 'Go']
+# print('Antes da listcomp = ', linguagens)
+# linguagens = [item.lower() for item in linguagens]
+# print('Depois da listcomp =', linguagens)
+
+# linguagens = '''Python Java JavaScript C C++ C# Swift Go Kotlin'''.split()
+# linguagens_java = [item for item in linguagens if 'Java' in item]
+# print(linguagens_java)
+
+# FUNÇÕES MAP() E FILTER()
+# print("Exemplo 1")
+# linguagens = '''Python Java JavaScript C C# C++ Swift Go Kotlin'''.split()
+# nova_lista = map(lambda x: x.lower(), linguagens)
+# print(f'A nova lista é = {nova_lista}')
+# nova_lista = list(nova_lista)
+# print(f'Agora sim é a nova lista = {nova_lista}')
+
+# print("\n\nExemplo 2")
+# numeros = [0, 1, 2, 3, 4, 5]
+# quadrados = list(map(lambda x: x*x, numeros))
+# print(f'Lista com o numero elevado a ele mesmo = {quadrados}')
+
+# numeros = list(range(0, 21))
+# numeros_pares = list(filter(lambda x: x % 2 == 0, numeros))
+# numeros_impares = list(filter(lambda x: x % 2 == 1, numeros))
+# print(numeros_pares)
+# print(numeros_impares)
+
+# TUPLAS
+# vogais = ('a', 'e', 'i', 'o', 'u')
+# print(f"Tipo do objeto vogais = {type(vogais)}")
+# for i, item in enumerate(vogais):
+#     print(f'Posição = {i}, valor = {item}')
+
+# vogais = ()
+# vogais.append('a')
+
+# vogais = ('a', 'e', 'i', 'o', 'u')
+# for item in enumerate(vogais):
+#     print(item)
+# print(tuple(enumerate(vogais)))
+# print(list(enumerate(vogais)))
