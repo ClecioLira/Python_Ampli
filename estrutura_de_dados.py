37c4a3d454021e1a086555d03c1425dc4f8563d2
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-12-19 08:55:11 -0300

    update

---
+++ estrutura_de_dados.py
@@ -0,0 +1,53 @@
+# OBJETOS DO TIPO SEQUÊNCIA: TEXTO, LISTAS E TUPLAS.
+# texto = "Aprendendo Python na disciplina de linguagem de programação."
+# print(f'Tamanho do texto = {len(texto)}')
+# print(f'Python in texto = {'Python' in texto}')
+# print(f'Quantidade de y no texto = {texto.count('y')}')
+# print(f'As 5 primeiras letras do texto são = {texto[0:6]}')
+# print(texto.upper())
+# print(texto.replace('i', 'XX'))
+# palavras = texto.split()
+# print(f'Palavras = {palavras}')
+# print(f'Quantidade de palavras = {len(palavras)}')
+
+# LISTAS
+# vogais = ['a', 'e', 'i', 'o', 'u']
+# for vogal in vogais:
+#     print(f'Posição = {vogais.index(vogal),} Valor = {vogal}')
+
+# vogais = []
+# print(f'Tipo do objeto vogais = {type(vogais)}')
+# vogais.append('a')
+# vogais.append('e')
+# vogais.append('i')
+# vogais.append('o')
+# vogais.append('u')
+# print(vogais)
+
+# frutas = ['maça', 'banana', 'uva', 'mamaõ', 'maça', 'abacaxi']
+# notas = [8.7, 5.2, 10, 3.5]
+# print('maça' in frutas)
+# print('abacate' in frutas)
+# print('abacate' not in frutas)
+# print(min(frutas))
+# print(max(notas))
+# print(frutas.count('maça'))
+# print(frutas + notas)
+# print(2 * frutas)
+
+# lista = ['Python', 30.61, 'Java', 51, ['a' , 'b', 20], 'maça']
+# print(f'Tamanho da lista = {len(lista)}')
+# for i, item in enumerate(lista):
+#     print(f'Posição = {i}, \t valor = {item}, \t tipo individual = {type(item)}')
+# print('\nEXEMPLOS DE SLICE:\n')
+# print("lista[1] = ", lista[1])
+# print("lista[0:2] = ", lista[0:2])
+# print("lista[:2] = ", lista[:2])
+# print("lista[3:5] = ", lista[3:5])
+# print("lista[3:6] = ", lista[3:6])
+# print("lista[3:] = ", lista[3:])
+# print("lista[-2] = ", lista[-2])
+# print("lista[-1] = ", lista[-1])
+# print("lista[4][1] = ", lista[4][1])
+
+# COMPREENSÃO DE LISTA
