d58bd96bcef0d3c21a50b3dc32f8f49dfb17babb
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-12-23 09:24:55 -0300

    update

--- algoritmo_de_ordenacao.py
+++ algoritmo_de_ordenacao.py
@@ -135,63 +135,97 @@
 # lista = [10, 9, 5, 8, 11, -1, 3]
 # print(executar_quicksort_2(lista))
 
-def executar_merge_sort(lista, inicio = 0, fim = None):
-    if not fim:
-        fim = len(lista)
+# def executar_merge_sort(lista, inicio = 0, fim = None):
+#     if not fim:
+#         fim = len(lista)
 
-    if fim - inicio > 1:
-        meio = (inicio + fim) // 2
-        executar_merge_sort(lista, inicio, meio)
-        executar_merge_sort(lista, meio, fim)
-        executar_merge(lista, inicio, meio, fim)
-    return lista
+#     if fim - inicio > 1:
+#         meio = (inicio + fim) // 2
+#         executar_merge_sort(lista, inicio, meio)
+#         executar_merge_sort(lista, meio, fim)
+#         executar_merge(lista, inicio, meio, fim)
+#     return lista
 
-def executar_merge(lista, inicio, meio, fim):
-    esquerda = lista[inicio:meio]
-    direita = lista[meio:fim]
-    topo_esquerda = topo_direita = 0
-    for p in range(inicio, fim):
-        if topo_esquerda >= len(esquerda):
-            lista[p] = direita[topo_direita]
-            topo_direita += 1
-        elif topo_direita >= len(direita):
-            lista[p] = esquerda[topo_esquerda]
-            topo_esquerda += 1
-        elif esquerda[topo_esquerda] < direita[topo_direita]:
-            lista[p] = esquerda[topo_esquerda]
-            topo_esquerda += 1
+# def executar_merge(lista, inicio, meio, fim):
+#     esquerda = lista[inicio:meio]
+#     direita = lista[meio:fim]
+#     topo_esquerda = topo_direita = 0
+#     for p in range(inicio, fim):
+#         if topo_esquerda >= len(esquerda):
+#             lista[p] = direita[topo_direita]
+#             topo_direita += 1
+#         elif topo_direita >= len(direita):
+#             lista[p] = esquerda[topo_esquerda]
+#             topo_esquerda += 1
+#         elif esquerda[topo_esquerda] < direita[topo_direita]:
+#             lista[p] = esquerda[topo_esquerda]
+#             topo_esquerda += 1
+#         else:
+#             lista[p] = direita[topo_direita]
+#             topo_direita += 1
+
+# def executar_busca_binaria(lista, valor):
+#     minimo = 0
+#     maximo = len(lista) - 1
+#     while minimo <= maximo:
+#         meio = (minimo + maximo) // 2
+#         if valor < lista[meio]:
+#             maximo = meio - 1
+#         elif valor > lista[meio]:
+#             minimo = meio + 1
+#         else:
+#             return True
+#     return False
+
+# def criar_lista_dedup_ordenada(lista):
+#     lista = [str(cpf).replace('.', '').replace('-', '') for cpf in lista]
+#     lista = [cpf for cpf in lista if len(cpf) == 11]
+#     lista = executar_merge_sort(lista)
+
+#     lista_dedup = []
+#     for cpf in lista:
+#         if not executar_busca_binaria(lista_dedup, cpf):
+#             lista_dedup.append(cpf)
+#     return lista_dedup
+
+# def testar_funcao(lista_cpfs):
+#     lista_dedup = criar_lista_dedup_ordenada(lista_cpfs)
+#     print(lista_dedup)
+
+# lista_cpfs = ['44444444444', '111.111.111-11', '11111111111', '222.222.222-22', '333.333.333-33', '22222222222', '444.44444']
+
+# print(testar_funcao(lista_cpfs))
+
+# def valor_lista():
+#     for i in range(1, 5):
+#         lista.append(int(input(f'Digite o valor da {i}° prova: ')))
+
+# def media():
+#     s = 0
+#     for i in range(len(lista)):
+#         s += lista[i]
+#     m = s / len(lista)
+#     return m
+
+# lista = []
+# valor_lista()
+# print(f'Sua média é: {media()}')
+
+def busca_binaria(lista, item):
+    prim = 0
+    ult = len(lista)
+    found = False
+
+    while prim <= ult and not found:
+        meio = (prim + ult) // 2
+        if lista[meio] == item:
+            found = True
         else:
-            lista[p] = direita[topo_direita]
-            topo_direita += 1
+            if item < lista[meio]:
+                ult = meio - 1
+            else:
+                prim = meio + 1
+    return found
 
-def executar_busca_binaria(lista, valor):
-    minimo = 0
-    maximo = len(lista) - 1
-    while minimo <= maximo:
-        meio = (minimo + maximo) // 2
-        if valor < lista[meio]:
-            maximo = meio - 1
-        elif valor > lista[meio]:
-            minimo = meio + 1
-        else:
-            return True
-    return False
-
-def criar_lista_dedup_ordenada(lista):
-    lista = [str(cpf).replace('.', '').replace('-', '') for cpf in lista]
-    lista = [cpf for cpf in lista if len(cpf) == 11]
-    lista = executar_merge_sort(lista)
-
-    lista_dedup = []
-    for cpf in lista:
-        if not executar_busca_binaria(lista_dedup, cpf):
-            lista_dedup.append(cpf)
-    return lista_dedup
-
-def testar_funcao(lista_cpfs):
-    lista_dedup = criar_lista_dedup_ordenada(lista_cpfs)
-    print(lista_dedup)
-
-lista_cpfs = ['44444444444', '111.111.111-11', '11111111111', '222.222.222-22', '333.333.333-33', '22222222222', '444.44444']
-
-print(testar_funcao(lista_cpfs))
+lista = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+print(busca_binaria(lista, 3))
